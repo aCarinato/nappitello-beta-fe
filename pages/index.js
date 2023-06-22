aa27e476944ac3e66a6f2d83f6a0a9724cf725dd
aCarinato <alessandro.carinato@gmail.com>
2023-06-22 16:27:38 +0200

    vercel commit 333

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 // components
-// import Payment from '../components/Payment';
 import ProductItem from '../components/Products/ProductItem';
 // own data
 import { products } from '../data/products';
@@ -7,7 +6,6 @@ import { products } from '../data/products';
 function HomePage() {
   return (
     <div className="flex-space-between">
-      {/* <Payment /> */}
       {products.map((product) => (
         <ProductItem key={product._id} product={product} />
       ))}
